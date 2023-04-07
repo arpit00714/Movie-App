e01fc48fceb7d536d910009fd6d0b01ee5750766
arpit00714 <arpitkhare14@gmail.com>
2023-04-07 16:32:58 +0530

    fetch request and receive data on app

--- src/App.js
+++ src/App.js
@@ -8,56 +8,69 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  
 
   const FetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
 
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://react-http-10f05-default-rtdb.firebaseio.com/movies.json"
+      );
       if (!response.ok) {
         throw new Error("Something went wrong");
       }
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_Data,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
 
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
-  },[]);
+  }, []);
 
   useEffect(() => {
     FetchMoviesHandler();
-   }, [FetchMoviesHandler]);
+  }, [FetchMoviesHandler]);
 
-   async function addMovieHandler(movie) {
-
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      "https://react-http-10f05-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
   }
 
   let content = <p>Found no movies.</p>;
 
-  if(movies.length > 0){
+  if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
   }
 
-  if(error){
-    content = <p>{error}</p>
+  if (error) {
+    content = <p>{error}</p>;
   }
 
-  if(isLoading){
+  if (isLoading) {
     content = <p>Loading...</p>;
   }
 
