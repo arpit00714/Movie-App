ffa754741372a50df1ef923c36d4d75e6d098fee
arpit00714 <arpitkhare14@gmail.com>
2023-04-07 14:41:44 +0530

    Add useEffect or useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -7,8 +7,9 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  
 
-  async function FetchMoviesHandler() {
+  const FetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
 
@@ -34,7 +35,12 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  },[]);
+
+  useEffect(() => {
+    FetchMoviesHandler();
+   }, [FetchMoviesHandler]);
+   
 
   let content = <p>Found no movies.</p>;
 
